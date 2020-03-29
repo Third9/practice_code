7bb1f2f8f7d249170ab7347b854e5ec4ee72cd95
Hyeock Kwon <dev.third9@gmail.com>
2020-03-29 17:34:39 +0900

    -. Code registration for gRPC practice : Registration for PB files : Add server and client code :: pb2 file is a pb build file. so do not register

--- gRPC/python/rpc_clients.py
+++ gRPC/python/rpc_clients.py
@@ -1,3 +1,27 @@
 # Created at 2020. 3. 1.
 
 __author__ = "hyeockkwon"
+
+# from __future__ import print_function
+import logging
+
+import grpc
+
+import practice_pb2
+import practice_pb2_grpc
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = practice_pb2_grpc.ChannelRPCStub(channel)
+        response = stub.TestConn(practice_pb2.ConnRequest(status_text='Success'))
+
+        print("TestConn status: {} / message: {}".format(response.status, response.message))
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
--- gRPC/python/rpc_servers.py
+++ gRPC/python/rpc_servers.py
@@ -1,3 +1,44 @@
 # Created at 2020. 3. 1.
 
 __author__ = "hyeockkwon"
+
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter server."""
+
+from concurrent import futures
+import logging
+
+import grpc
+
+import practice_pb2
+import practice_pb2_grpc
+
+
+class ChannelRPC(practice_pb2_grpc.ChannelRPCServicer):
+
+    def TestConn(self, request, context):
+        return practice_pb2.ConnResponse(status=200, message="Connect {}".format(request.status_text))
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    practice_pb2_grpc.add_ChannelRPCServicer_to_server(ChannelRPC(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
